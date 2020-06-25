8110adeee7101494cea9bb13f6a07b47f8df3684
Michal Stachnik <michal@michals-mini.myfiosgateway.com>
2020-06-25 15:26:56 -0400

    testing

--- pages/api/test.js
+++ pages/api/test.js
@@ -1,5 +1,7 @@
 export default (req, res) => {
+  const connection_string = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0-ii9ft.mongodb.net/farm-fresh?retryWrites=true&w=majority`;
+
   res.statusCode = 200;
   res.setHeader('Content-Type', 'application/json');
-  res.end(JSON.stringify({ name: 'test ok' }));
+  res.end(JSON.stringify({ name: 'test ok', conn: connection_string }));
 };
