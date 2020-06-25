dd16d82dfa3e98b64369b1152205d6f3301603b5
Michal Stachnik <michal@michals-mini.myfiosgateway.com>
2020-06-25 15:35:42 -0400

    wtf

--- pages/api/test.js
+++ pages/api/test.js
@@ -2,6 +2,9 @@ export default (req, res) => {
   const connection_string = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0-ii9ft.mongodb.net/farm-fresh?retryWrites=true&w=majority`;
   const mb = process.env.MAPBOX_KEY;
 
+  const user = process.env.DB_USER;
+  const pass = process.env.DB_PASS;
+
   res.statusCode = 200;
   res.setHeader('Content-Type', 'application/json');
   res.end(
@@ -9,6 +12,8 @@ export default (req, res) => {
       name: 'test ok',
       conn: connection_string,
       mb,
+      user,
+      pass,
     })
   );
 };
