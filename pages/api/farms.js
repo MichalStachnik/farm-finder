5e3fb16898c70ed82f469078db74eae0ab98db79
Michal Stachnik <michal@michals-mini.myfiosgateway.com>
2020-07-05 09:27:51 -0400

    made connectToDb its own exported function

--- pages/api/farms.js
+++ pages/api/farms.js
@@ -1,37 +1,12 @@
-const url = require('url');
-const MongoClient = require('mongodb').MongoClient;
+import { connectToDb } from '../../services/connectToDb.service';
+
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0-ii9ft.mongodb.net/farm-fresh?retryWrites=true&w=majority`;
-
-// Create cached connection variable
-let cachedDb = null;
-
-// A function for connecting to MongoDB,
-// taking a single parameter of the connection string
-async function connectToDatabase(uri) {
-  // If the database connection is cached,
-  // use it instead of creating a new connection
-  if (cachedDb) {
-    return cachedDb;
-  }
-
-  // If no connection is cached, create a new one
-  const client = await MongoClient.connect(uri, { useNewUrlParser: true });
-
-  // Select the database through the connection,
-  // using the database path of the connection string
-  const dbName = url.parse(uri).pathname.substr(1);
-  const db = await client.db(dbName);
-
-  // Cache the database connection and return the connection
-  cachedDb = db;
-  return db;
-}
 
 // The main, exported, function of the endpoint,
 // dealing with the request and subsequent response
 module.exports = async (req, res) => {
   // Get a database connection, cached or otherwise,
-  const db = await connectToDatabase(uri);
+  const db = await connectToDb(uri);
 
   // Select the "farms" collection from the database
   const collection = await db.collection('farms');
